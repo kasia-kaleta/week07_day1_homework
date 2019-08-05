82196c7ceee1686fe7cd7eb11b9e9764c96f4216
kat3005 <kasia.alicja@gmail.com>
2019-08-05 21:33:56 +0100

    Adds completed mvp

--- src/app.js
+++ src/app.js
@@ -2,11 +2,21 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
-  		el: "#app",
-  		data: {
-        items: ['Clean windows', 'Buy wine', 'Throw away the rubbish'],
-        newItem: ''
-      },
-    methods: {}
+    el: "#app",
+    data: {
+      items: [
+      {name: 'Clean windows', priority: 'low'},
+      {name: 'Buy wine', priority: 'high'},
+      {name: 'Throw away the rubbish', priority: 'high'}
+      ],
+      newItem: ''
+    },
+    methods: {
+      saveNewItem: function () {
+        event.preventDefault();
+        this.items.push(this.newItem);
+        this.newItem = '';
+      }
+    }
   });
 });
